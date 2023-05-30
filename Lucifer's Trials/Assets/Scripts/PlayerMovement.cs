e111a29b228b89b63ea2ed9f914ae37ed144a120
Sethstofer <seth.aaron14@gmail.com>
2023-05-29 18:11:41 -0700

    Player Animation Begun
    Spliced all images for every moveset of the warrior; created animations; created and setup an animation controller for the player that uses these animations; updated the PlayerMovement.cs to allow for sprite change in gameplay.

--- Lucifer's Trials/Assets/Scripts/PlayerMovement.cs
+++ Lucifer's Trials/Assets/Scripts/PlayerMovement.cs
@@ -5,24 +5,74 @@ using UnityEngine.EventSystems;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private float speed = 8.0f;
+    private float speed = 4.0f;
     private float horizontal;
     private float vertical;
     private bool isDashing = false;
     private float dashDuration = 0.25f;
     private float curDuration;
     private string facedDirection = "Down";
+    Vector2 moveInput = Vector2.zero;
+    Animator animator;
+    SpriteRenderer spriteRenderer;
 
     [SerializeField] private Rigidbody2D rb;
+
+    private enum PlayerStates { IDLE, WALK, ATTACK }
+    PlayerStates currentState;
+    PlayerStates CurrentState
+    {
+        set
+        {
+            currentState = value;
+
+            // set which animation the sprite uses
+            switch(currentState)
+            {
+                case PlayerStates.IDLE:
+                    animator.Play("Idle");
+                    break;
+                case PlayerStates.WALK:
+                    animator.Play("Walk");
+                    break;
+                // case PlayerStates.ATTACK:
+                //     animator.Play("Attack");
+                //     break;
+            }
+        }
+    }
 
     public string GetFacedDirection()
     {
         return this.facedDirection;
     }
 
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     // Update is called once per frame
     void Update()
     {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+
+        // determine sprite direction
+        if ((horizontal != 0) || (vertical != 0))
+        {
+            CurrentState = PlayerStates.WALK;
+            animator.speed = speed / 2;
+            animator.SetFloat("MoveX", horizontal);
+            animator.SetFloat("MoveY", vertical);
+        }
+        else
+        {
+            CurrentState = PlayerStates.IDLE;
+            animator.speed = 1;
+        }
+
         if (Input.GetKeyDown("space") && rb.velocity != Vector2.zero)
         {
             Debug.Log("Space Pressed");
@@ -32,9 +82,6 @@ public class PlayerMovement : MonoBehaviour
 
         if (!isDashing)
         {
-            horizontal = Input.GetAxisRaw("Horizontal");
-            vertical = Input.GetAxisRaw("Vertical");
-
             if (horizontal == -1 && vertical == 0)
             {
                 //Debug.Log("Facing Left");
