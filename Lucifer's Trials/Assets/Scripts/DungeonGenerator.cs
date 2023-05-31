29e189132dcdb380267d6d2d07f91bccc8e6dfc2
Russell Reich <RustyReich@gmail.com>
2023-05-31 12:34:11 -0700

    Wall objects are placed in the center of tiles

--- Lucifer's Trials/Assets/Scripts/DungeonGenerator.cs
+++ Lucifer's Trials/Assets/Scripts/DungeonGenerator.cs
@@ -431,8 +431,7 @@ public class DungeonGenerator : IRoomGenerator
 
                     GameObject wallObject = new GameObject("Wall");
                     wallObject.transform.SetParent(collisionLayer.transform);
-                    wallObject.transform.position = _tileMaps["Collision"].CellToWorld(new Vector3Int(x, y, 0));
-                    wallObject.transform.localScale = new Vector3(1, 1, 0);
+                    wallObject.transform.position = _tileMaps["Collision"].CellToWorld(new Vector3Int(x, y, 0)) + new Vector3(0.5f, 0.5f, 0);
                     wallObject.layer = LayerMask.NameToLayer("Wall");
 
                 }
