fd250f272dce34446535b134b2b1e3362a9ffded
Sethstofer <seth.aaron14@gmail.com>
2023-06-05 21:26:46 -0700

    Fixed Spelling

--- Lucifer's Trials/Assets/Scripts/Collectables/getType.cs
+++ Lucifer's Trials/Assets/Scripts/Collectables/getType.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class getType : MonoBehaviour
+public class GetType : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
