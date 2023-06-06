8ca3d1b72dede8abb3d7c92aacedba9feb88041d
Sethstofer <seth.aaron14@gmail.com>
2023-06-05 21:31:12 -0700

    File Spelling & Location Correction

--- Lucifer's Trials/Assets/Scripts/move.cs
+++ Lucifer's Trials/Assets/Scripts/move.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class move : MonoBehaviour
+public class Move : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
