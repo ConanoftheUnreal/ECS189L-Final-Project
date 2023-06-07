cc75a5602ce05bcde44d842f0d1fc8c2f090d985
Conar Abramson-Davis <conarlp@gmail.com>
2023-06-06 19:28:22 -0700

    Added enemy health bar, for some reason no color appears.

---
+++ Lucifer's Trials/Assets/Scripts/Enemy/HealthBarController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarController : MonoBehaviour
+{
+    [SerializeField] private Slider healthSlider;
+    [SerializeField] private Color low;
+    [SerializeField] private Color high;
+    [SerializeField] private Vector3 offset;
+
+    public void SetHealth(int health, int maxHealth)
+    {
+        healthSlider.value = health;
+        healthSlider.maxValue = maxHealth;
+
+        healthSlider.fillRect.GetComponentInChildren<Image>().color = Color.Lerp(this.low, this.high, this.healthSlider.normalizedValue);
+    }
+
+    void Update()
+    {
+        healthSlider.transform.position = Camera.main.WorldToScreenPoint(transform.parent.position + this.offset);
+        Debug.Log(healthSlider.value);
+    }
+}
