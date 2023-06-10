66de3c71332cf0c86fb2582f60c3234a1be7f17f
Russell <RustyReich@gmail.com>
2023-06-10 12:28:46 -0700

    Make BlackBorder outer corners look cleaner

--- Lucifer's Trials/Assets/Scripts/Procedural Generation/DungeonTileset.cs
+++ Lucifer's Trials/Assets/Scripts/Procedural Generation/DungeonTileset.cs
@@ -60,7 +60,7 @@ public sealed class DungeonTileset : ITileset
         instance.PairNameAndTile("TopWall_Right", instance.GetTileByFilename("Dungeon_Tileset_3"));
 
         instance.PairNameAndTile("BlackBorder_Top", instance.GetTileByFilename("Dungeon_Tileset_43"));
-        instance.PairNameAndTile("BlackBorder_Bottom", instance.GetTileByFilename("Dungeon_Tileset_108"));
+        instance.PairNameAndTile("BlackBorder_Bottom", instance.GetTileByFilename("Dungeon_Tileset_110"));
         instance.PairNameAndTile("BlackBorder_Right", instance.GetTileByFilename("Dungeon_Tileset_26"));
         instance.PairNameAndTile("BlackBorder_Left", instance.GetTileByFilename("Dungeon_Tileset_27"));
         instance.PairNameAndTile("BlackBorder_BottomLeft", instance.GetTileByFilename("Dungeon_Tileset_42"));
@@ -78,10 +78,15 @@ public sealed class DungeonTileset : ITileset
         instance.PairNameAndTile("BigDoor_BottomLeft", instance.GetTileByFilename("Dungeon_Tileset_85"));
         instance.PairNameAndTile("BigDoor_BottomRight", instance.GetTileByFilename("Dungeon_Tileset_86"));
 
-        instance.PairNameAndTile("Exit_Left", instance.GetTileByFilename("Dungeon_Tileset_110"));
-        instance.PairNameAndTile("Exit_Right", instance.GetTileByFilename("Dungeon_Tileset_116"));
-        instance.PairNameAndTile("Exit_Up", instance.GetTileByFilename("Dungeon_Tileset_114"));
-        instance.PairNameAndTile("Exit_Down", instance.GetTileByFilename("Dungeon_Tileset_115"));
+        instance.PairNameAndTile("Exit_Left", instance.GetTileByFilename("Dungeon_Tileset_112"));
+        instance.PairNameAndTile("Exit_Right", instance.GetTileByFilename("Dungeon_Tileset_118"));
+        instance.PairNameAndTile("Exit_Up", instance.GetTileByFilename("Dungeon_Tileset_116"));
+        instance.PairNameAndTile("Exit_Down", instance.GetTileByFilename("Dungeon_Tileset_117"));
+
+        instance.PairNameAndTile("BlackBorderCorner_TopLeft", instance.GetTileByFilename("Dungeon_Tileset_59"));
+        instance.PairNameAndTile("BlackBorderCorner_TopRight", instance.GetTileByFilename("Dungeon_Tileset_61"));
+        instance.PairNameAndTile("BlackBorderCorner_BottomLeft", instance.GetTileByFilename("Dungeon_Tileset_103"));
+        instance.PairNameAndTile("BlackBorderCorner_BottomRight", instance.GetTileByFilename("Dungeon_Tileset_104"));
 
     }
 
