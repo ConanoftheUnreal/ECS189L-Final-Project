cff6d68884210d00aeeb62f289d36ffeaa3576eb
Russell <RustyReich@gmail.com>
2023-06-11 15:38:23 -0700

    Remove the need to press Space to generate level

--- Lucifer's Trials/Assets/Scripts/Procedural Generation/ManualRoomGeneration.cs
+++ Lucifer's Trials/Assets/Scripts/Procedural Generation/ManualRoomGeneration.cs
@@ -30,44 +30,21 @@ public class ManualRoomGeneration : MonoBehaviour
         environmentUpdater = enemiesEnv.GetComponent<Polarith.AI.Package.EnvironmentUpdater>();
         environmentUpdater.GameObjectCollections[0] = GameObject.Find("Enemies");
 
+        _levelManager = _levelGenerator.Generate().roomObject.GetComponent<LevelManager>();
+        
+        CameraController cc = _camera.gameObject.GetComponent<CameraController>();
+        cc.SnapToRoom(_levelManager.currentNode.room);
+
+        Vector2 entranceLocation = _levelManager.currentNode.room.exitPaths[LevelGenerator.ENTRANCE_EXIT_ID].entranceLocation;
+        GameObject player = GameObject.Find("Player");
+        player.transform.position = new Vector3(entranceLocation.x, entranceLocation.y, 0);
+        player.transform.SetParent(_levelManager.currentNode.room.roomObject.transform);
+        player.GetComponent<SpriteRenderer>().enabled = true;
+
     }
 
     public void Update()
-    {
-
-        if (Input.GetButtonDown("Jump"))
-        {
-
-            if (GameObject.Find("Root") != null)
-            {
-                Destroy(GameObject.Find("Root"));
-            }
-            else
-            {
-
-                _levelManager = _levelGenerator.Generate().roomObject.GetComponent<LevelManager>();
-                
-                CameraController cc = _camera.gameObject.GetComponent<CameraController>();
-                cc.SnapToRoom(_levelManager.currentNode.room);
-
-                Vector2 entranceLocation = _levelManager.currentNode.room.exitPaths[LevelGenerator.ENTRANCE_EXIT_ID].entranceLocation;
-                GameObject player = GameObject.Find("Player");
-                player.transform.position = new Vector3(entranceLocation.x, entranceLocation.y, 0);
-                player.transform.SetParent(_levelManager.currentNode.room.roomObject.transform);
-                player.GetComponent<SpriteRenderer>().enabled = true;
-
-            }
-
-        }
-
-        
-        if (Input.GetButtonDown("Fire2"))
-        {
-
-            Debug.Log(_levelManager.GetCenterOfCurrentRoom());
-
-        }
-        
+    {   
 
     }
 
