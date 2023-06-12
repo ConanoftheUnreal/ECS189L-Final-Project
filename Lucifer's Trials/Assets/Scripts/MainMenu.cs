6621398c71a0c0ef1b12b38c2c479659a34c1171
Steven To <stto@ucdavis.edu>
2023-06-11 23:32:08 -0700

    added sfx to main menu + fixed enemy sound bug

--- Lucifer's Trials/Assets/Scripts/MainMenu.cs
+++ Lucifer's Trials/Assets/Scripts/MainMenu.cs
@@ -7,11 +7,13 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
+        FindObjectOfType<SoundManager>().PlaySoundEffect("Good Select");
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void QuitGame()
     {
+        FindObjectOfType<SoundManager>().PlaySoundEffect("Good Select");
         Debug.Log("QUIT");
         Application.Quit();
     }
