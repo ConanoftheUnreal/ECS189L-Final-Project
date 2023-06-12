45c73f784607bc1ad1ab2563eb7906ec241ca379
Sethstofer <seth.aaron14@gmail.com>
2023-06-12 16:48:10 -0700

    Projectile Script Fix
    Just some relocation of code for concision.

--- Lucifer's Trials/Assets/Scripts/Player/ProjectileScript.cs
+++ Lucifer's Trials/Assets/Scripts/Player/ProjectileScript.cs
@@ -14,7 +14,7 @@ public class ProjectileScript : MonoBehaviour
     public void OnTriggerEnter2D(Collider2D col)
     {
         // All projectiles that hit a wall should be destroyed.
-        if (col.tag == "Wall")
+        if ( (col.tag == "Wall") || (col.tag == "PlayerAttack") || (this.tag == "Projectile" && col.tag == "EnemyHitbox") )
         {
             Destroy(this.gameObject);
             AfterEffect();
@@ -31,12 +31,6 @@ public class ProjectileScript : MonoBehaviour
                 Destroy(this.gameObject);
                 AfterEffect();
             }
-        }
-
-        if (col.tag == "PlayerAttack")
-        {
-            Destroy(this.gameObject);
-            AfterEffect();
         }
     }
 
