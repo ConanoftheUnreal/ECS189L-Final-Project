f8daa43f8d5ed98f61b0bda01abdf939716391de
Conar Abramson-Davis <conarlp@gmail.com>
2023-06-12 21:13:48 -0700

    Fix bugs with money, health collisions and the issue with losing the scriptable object data.

--- Lucifer's Trials/Assets/Scripts/Player/PlayerController.cs
+++ Lucifer's Trials/Assets/Scripts/Player/PlayerController.cs
@@ -180,7 +180,6 @@ public class PlayerController : MonoBehaviour
 
     void OnDestroy()
     {
-        this.playerStats.playerType = this.gameObject.GetComponent<PlayerAnimationController>().GetPlayerType();
         switch(this.playerStats.playerType)
         {
             case PlayerType.WARRIOR:
--- Lucifer's Trials/Assets/Scripts/SwitchSorceress.cs
+++ Lucifer's Trials/Assets/Scripts/SwitchSorceress.cs
@@ -6,6 +6,7 @@ using Lucifer;
 public class SwitchSorceress : MonoBehaviour
 {
     private GameObject player;
+    [SerializeField] private PlayerStats playerStats;
 
     void OnTriggerEnter2D (Collider2D obj)
     {
@@ -24,6 +25,7 @@ public class SwitchSorceress : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.E) && this.player != null)
         {
+            this.playerStats.playerType = PlayerType.SORCERESS;
             this.player.GetComponent<PlayerAnimationController>().SetClass(PlayerType.SORCERESS);
             this.player.GetComponent<PlayerController>().newClass();
             this.player.GetComponent<PlayerAnimationController>().StartNewAnimation();
